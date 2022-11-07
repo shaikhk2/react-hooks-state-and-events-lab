9d0af430ea1723888916ed87299c6829727c0e71
Kulsum Shaikh <shaikhkulsum233@gmail.com>
2022-11-06 18:30:20 -0600

    commit tree 984b6cb7c57939afa229b34347de218e11884e7e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addToCart, setAddToCart] = useState(false);
+ 
+  const [liClassName, setLiClassName] = useState("");
+
+  function cartClick() {
+    console.log(addToCart);
+    console.log(!addToCart);
+    // setAddToCart((addToCart) => !addToCart)
+    setAddToCart(!addToCart)
+    console.log(addToCart);
+    console.log( addToCart ? "in-cart" : "" );
+    setLiClassName( addToCart ? "in-cart" : "" )
+  }
+
+
+  const cartClass = addToCart ? "Remove from Cart" : "Add to Cart";
+  //console.log(cartClass);
+  //why is the clasname change not working?!?! 
+ 
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={cartClick} className="add">{cartClass}</button>
     </li>
   );
 }
